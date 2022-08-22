4704de61c2f18695713746aafad237858179a2c0
Baris <s.barissonmez@gmail.com>
2022-08-22 21:54:55 +0300

    Update generate_topology.py

--- generate_topology.py
+++ generate_topology.py
@@ -25,3 +25,92 @@ NX_LAYER_SORT_ORDER = (
     'spine',
     'access-switch'
 )
+
+
+nr = InitNornir(config_file=NORNIR_CONFIG_FILE)
+
+icon_capability_map = {
+    'router': 'router',
+    'switch': 'switch',
+    'bridge': 'switch',
+    'station': 'host'
+
+
+icon_model_map = {
+    'CSR1000V': 'router',
+    'Nexus': 'switch',
+    'IOSXRv': 'router',
+    'IOSv': 'switch',
+    '2901': 'router',
+    '2911': 'router',
+    '2921': 'router',
+    '2951': 'router',
+    '4321': 'router',
+    '4331': 'router',
+    '4351': 'router',
+    '4421': 'router',
+    '4431': 'router',
+    '4451': 'router',
+    '2960': 'switch',
+    '3750': 'switch',
+    '3850': 'switch',
+}
+
+
+interface_full_name_map = {
+    'Eth': 'Ethernet',
+    'Fa': 'FastEthernet',
+    'Gi': 'GigabitEthernet',
+    'Te': 'TenGigabitEthernet',
+}
+
+
+def if_fullname(ifname):
+    for k, v in interface_full_name_map.items():
+        if ifname.startswith(v):
+            return ifname
+        if ifname.startswith(k):
+            return ifname.replace(k, v)
+    return ifname
+
+
+def if_shortname(ifname):
+    for k, v in interface_full_name_map.items():
+        if ifname.startswith(v):
+            return ifname.replace(v, k)
+    return ifname
+
+
+def get_icon_type(device_cap_name, device_model=''):
+    """
+    Device icon selection function. Selection order:
+    - LLDP capabilities mapping.
+    - Device model mapping.
+    - Default 'unknown'.
+    """
+    if device_cap_name:
+        icon_type = icon_capability_map.get(device_cap_name)
+        if icon_type:
+            return icon_type
+    if device_model:
+        # Check substring presence in icon_model_map keys
+        # string until the first match
+        for model_shortname, icon_type in icon_model_map.items():
+            if model_shortname in device_model:
+                return icon_type
+    return 'unknown'
+
+
+def get_node_layer_sort_preference(device_role):
+    """Layer priority selection function
+    Layer sort preference is designed as numeric value.
+    This function identifies it by NX_LAYER_SORT_ORDER
+    object position by default. With numeric values,
+    the logic may be improved without changes on NeXt app side.
+    0(null) results undefined layer position in NeXt UI.
+    Valid indexes start with 1.
+    """
+    for i, role in enumerate(NX_LAYER_SORT_ORDER, start=1):
+        if device_role == role:
+            return i
+    return 1
